ba52751bccb121cc52f5012f472783fa01ce1905
boydclifton <bstinnett65@gmail.com>
2025-03-13 11:36:11 -0400

    3rd revision

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,4 @@
 const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").textContent = currentYear;
-alert(document.lastModified);
-document.getElementById("lastModified").textContent = document.lastModified;
+let lastModifiedDate = document.lastModified;
+document.getElementById("lastModified").textContent = lastModifiedDate;
