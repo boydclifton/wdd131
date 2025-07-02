54aa60419be381555af1e307dee3d4bacfc1ed13
boydclifton <bstinnett65@gmail.com>
2025-07-02 11:12:36 -0400

    2nd attempt

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,4 @@
-// const currentYear = new Date().getFullYear();
-// document.getElementById("currentyear").textContent = currentYear;
-// let lastModifiedDate = document.lastModified;
-// document.getElementById("lastModified").textContent = lastModifiedDate;
+
 
 
 const currentYear = document.querySelector("#currentyear");
