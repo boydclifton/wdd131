bad4b366975b3e095c661de9d3640d16aa774593
boydclifton <bstinnett65@gmail.com>
2025-07-02 11:05:58 -0400

    first try week 1

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,20 @@
-const currentYear = new Date().getFullYear();
-document.getElementById("currentyear").textContent = currentYear;
-let lastModifiedDate = document.lastModified;
-document.getElementById("lastModified").textContent = lastModifiedDate;
+// const currentYear = new Date().getFullYear();
+// document.getElementById("currentyear").textContent = currentYear;
+// let lastModifiedDate = document.lastModified;
+// document.getElementById("lastModified").textContent = lastModifiedDate;
+
+
+const currentYear = document.querySelector("#currentyear");
+const today= new Date();
+currentYear.innerHTML = today.getFullYear();
+
+const lastModifiedDate = document.lastModified;
+lastModified.innerHTML = lastModifiedDate
+
+
+
+
+
+
+
+
