ee00fe7d3831f29ee5cace6159d4c1f4554b3c0e
boydclifton <bstinnett65@gmail.com>
2025-09-15 11:46:53 -0400

    1st snapshot

---
+++ scripts/temples.js
@@ -0,0 +1,19 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+const text = document.querySelector("text");
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+
+});
+
+const currentYear = document.querySelector("#currentyear");
+const today = new Date();
+currentYear.innerHTML = today.getFullYear();
+
+const lastModifiedDate = document.lastModified;
+lastModified.innerHTML = lastModifiedDate;
+
+
+
