1c54f12880341c8c67455758b5fb0b4fde325c04
boydclifton <bstinnett65@gmail.com>
2025-09-20 17:48:55 -0400

    2nd iteration

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,42 +1,5 @@
 
 
-// const input = document.querySelector("#favchap");
-// const button = document.querySelector("button");
-// const list = document.querySelector("#list");
-
-
-// button.addEventListener("click", function () {
-  
-//   if (input.value.trim() === "") {
-//     alert("Please enter a chapter name before adding."); 
-//     input.focus(); 
-//     return; 
-//   }
-
-  
-//   const li = document.createElement("li");
-//   const deleteButton = document.createElement("button");
-
-//   li.textContent = input.value; 
-//   deleteButton.textContent = "❌"; 
-//   li.append(deleteButton); 
-//   list.append(li); 
-
-//   input.value = "";
-
-  
-//   input.focus();
-// });
-
-
-// list.addEventListener("click", function (e) {
-//   if (e.target.tagName === "BUTTON") {
-//     const li = e.target.parentElement;
-//     list.removeChild(li);
-//   }
-// });
-
-
 const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("#list");
--- week02/scripts/debugging.js
+++ week02/scripts/debugging.js
@@ -6,10 +6,10 @@ const PI = 3.14159;
 
 let radius = 10;
 area = PI * radius * radius;
-radiusOutput = radius;
-areaOutput = area;
+radiusOutput.textContent = radius;
+areaOutput.textContent = area;
 
 radius = 20;
 area = PI * radius * radius;
-radiusOutput.textContent = radius;
-areaOutput.textContent = area;
+radiusOutput = radius;
+areaOutput = area;
