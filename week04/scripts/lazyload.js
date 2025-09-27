7c6c4db76ca4d330e9345fc37840cc88819a5cc3
boydclifton <bstinnett65@gmail.com>
2025-09-27 14:04:57 -0400

    all recent work

--- week04/scripts/lazyload.js
+++ week04/scripts/lazyload.js
@@ -1,21 +1,6 @@
-
-
-
 const currentYear = document.querySelector("#currentyear");
 const today= new Date();
 currentYear.innerHTML = today.getFullYear();
 
 const lastModifiedDate = document.lastModified;
 lastModified.innerHTML = lastModifiedDate
-
-
-
-
-
-
-
-
-
-
-
-
