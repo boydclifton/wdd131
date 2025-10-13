42d857970f63867a74b90ee138a192b88a841620
boydclifton <bstinnett65@gmail.com>
2025-10-13 12:30:35 -0400

    changed the formset ids so that they're more specific

--- scripts/form.js
+++ scripts/form.js
@@ -1,28 +1,27 @@
-
-
-
 const products = [
-  { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
-  { id: "fc-2050", name: "power laces", averagerating: 4.7 },
-  { id: "fs-1987", name: "time circuits", averagerating: 3.5 },
-  { id: "ac-2000", name: "low voltage reactor", averagerating: 3.9 },
-  { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 }
+  { id: "fc-1888", name: "Flux Capacitor", averageRating: 4.5 },
+  { id: "fc-2050", name: "Power Laces", averageRating: 4.7 },
+  { id: "fs-1987", name: "Time Circuits", averageRating: 3.5 },
+  { id: "ac-2000", name: "Low Voltage Reactor", averageRating: 3.9 },
+  { id: "jj-1969", name: "Warp Equalizer", averageRating: 5.0 }
 ];
 
-const selectElement = document.getElementById("name");
+
+const selectElement = document.getElementById("productName");
+
 
 products.forEach(product => {
   let option = document.createElement("option");
-  option.value = product.id;   // 
-  option.textContent = product.name; 
+  option.value = product.id;
+  option.textContent = product.name;
   selectElement.appendChild(option);
 });
 
 
-
 const currentYear = document.querySelector("#currentyear");
-const today= new Date();
+const today = new Date();
 currentYear.innerHTML = today.getFullYear();
 
-const lastModifiedDate = document.lastModified;
-lastModified.innerHTML = lastModifiedDate
+
+const lastModified = document.querySelector("#lastModified");
+lastModified.innerHTML = document.lastModified;
